c971a2b843aa31364ad62797e1abbf70200eb59f
AL02029130 <RQ4KCFQKK2@AL02029130.local>
2022-11-01 11:36:15 +0700

    -Update SampleFramework file

---
+++ SampleFramework/SampleFramwork.xcframework/ios-arm64_x86_64-simulator/SampleFramwork.framework/Headers/SampleFramwork.h
@@ -0,0 +1,18 @@
+//
+//  SampleFramwork.h
+//  SampleFramwork
+//
+//  Created by AL02029130 on 31/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFramwork.
+FOUNDATION_EXPORT double SampleFramworkVersionNumber;
+
+//! Project version string for SampleFramwork.
+FOUNDATION_EXPORT const unsigned char SampleFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramwork/PublicHeader.h>
+
+
